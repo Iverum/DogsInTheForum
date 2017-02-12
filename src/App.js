dab0355e1f6bcfb4ea45b4f08bcf27601290cf56
Blake Hair <blake.hair@gmail.com>
2017-02-12 00:02:53 -0700

    Added firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import firebase from 'firebase'
+import config from './config'
 import './App.css'
 import store from './store'
 import Board from './components/Board'
@@ -9,7 +11,27 @@ import Thread from './components/Thread'
 
 const history = syncHistoryWithStore(browserHistory, store)
 
+console.log('Firebase', firebase)
+
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(config)
+    const firebaseAuth = firebase.auth()
+    firebaseAuth.getRedirectResult()
+      .then(result => {
+        if (firebaseAuth.currentUser) {
+          console.log('HAS USER', firebaseAuth.currentUser.displayName)
+        } else {
+          var provider = new firebase.auth.GoogleAuthProvider();
+          provider.addScope('profile');
+          provider.addScope('email');
+          firebase.auth().signInWithRedirect(provider)
+        }
+      })
+  }
+
+
   render() {
     return (
       <div className="App row">
